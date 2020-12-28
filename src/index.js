7142000c8fe663b7d2f9c42124179f0a4a0d28db
prashant1682 <prashant009@gmail.com>
2020-12-28 11:06:47 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Prashant";
+const lname = "Chacko";
+const number = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your lucky number is {number}.</p>
+    <p>Your random lucky number is {Math.floor(Math.random() * 10)}.</p>
+  </div>,
+  document.getElementById("root")
+);
